3725099309723d6245de294e818e6c40fe98f08b
Aman <amanchandinc@gmail.com>
2024-11-30 22:52:57 +0545

    Feat: Added easter eggs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="w-full border-t-2 border-neutral-800 absolute bottom-0 left-0 right-0 py-4 bg-background text-neutral-400 text-sm select-none">
+    <footer className="w-full border-t-2 border-neutral-800 absolute bottom-0 left-0 right-0 py-4 bg-background text-neutral-400 text-xs lg:text-sm select-none">
       <div className="max-w-[500px] lg:max-w-[750px] mx-auto flex items-center justify-between">
         <p>Aman Chand © {new Date().getFullYear()}</p>
         <p>
